5bc84bbcdbb3450184dea72af4e0b8eced148ba5
Yiğit Varlı <44034966+yigitv4rli@users.noreply.github.com>
2020-01-11 23:34:45 +0300

    Add files via upload

---
+++ the4_tester.py
@@ -0,0 +1,28 @@
+import the4
+import time
+
+start_time = time.time()
+
+wrong_count = 0
+with open("the4_data.json","r") as file:
+    lines = file.readlines()
+    for i in range(len(lines)-1):
+        lines[i] = lines[i][:-1]
+    for line in lines:
+        data = line.split(":::")
+        data[0] = eval(data[0])
+        data[1] = eval(data[1])
+        result = the4.chalchiuhtlicue(data[0])
+        
+        if result == data[1]:
+            print("Succesful")
+        elif result != data[1]:
+            print("Failed on {}".format(data[0]))
+            print("Your work found: {}".format(result))
+            print("It should have been {}.".format(data[1]))
+            wrong_count += 1
+
+end_time = time.time()
+
+print("Execution time is: {}".format(end_time-start_time))
+print("In {} cases, you've failed {} times.".format(len(lines),wrong_count))
