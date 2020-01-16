a0547affc15b7b72144eb67b53066d2b8e257c97
GoktugEk <58266670+GoktugEk@users.noreply.github.com>
2020-01-16 14:29:08 +0300

    Add files via upload

--- the4_tester.py
+++ the4_tester.py
@@ -1,36 +1,28 @@
 import the4
 import time
-
-def dictify(nested_lst):
-    out_lst=[]
-    for lst in nested_lst:
-        Dict={}
-        for e in lst:
-            Dict[e]=None
-        out_lst.append([Dict])
-    return out_lst
+import copy
 
 start_time = time.time()
 
 wrong_count = 0
 with open("the4_data.json","r") as file:
-    lines = file.readlines()
-    for i in range(len(lines)-1):
-        lines[i] = lines[i][:-1]
+    lines = file.read().splitlines()
     for line in lines:
         data = line.split(":::")
         data[0] = eval(data[0])
+        original_data = copy.deepcopy(data[0])
         data[1] = eval(data[1])
         for i in range(len(data[1])) :
             data[1][i].sort()
         result = the4.chalchiuhtlicue(data[0])
         for k in range(len(result)) :
             result[k].sort()
-
-        if dictify(result) == dictify(data[1]):
-            print("Succesful")
-        elif dictify(result) != dictify(data[1]):
-            print("Failed on {}".format(data[0]))
+        
+        
+        if result == data[1] and lines.index(line)%100 == 0:
+            print("Succesful ({}/{})".format(lines.index(line),len(lines)))
+        elif result != data[1]:
+            print("Failed on {}".format(original_data))
             print("Your work found: {}".format(result))
             print("It should have been {}.".format(data[1]))
             wrong_count += 1
